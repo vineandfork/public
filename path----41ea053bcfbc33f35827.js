a7174e2bac00f5c6c585f32a8157dec8b6d1bb90
Alex Thomsen <alex.scott.thomsen@gmail.com>
2018-11-24 11:47:42 -0800

    That nue nue with the logo

---
+++ path----41ea053bcfbc33f35827.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{102:function(t,e){t.exports={data:{site:{siteMetadata:{title:"vine & fork"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----41ea053bcfbc33f35827.js.map
