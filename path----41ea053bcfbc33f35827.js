99bfb55d1d300eff9da081d070bf3017cc5060da
Alex Thomsen <alex.scott.thomsen@gmail.com>
2019-10-20 17:33:17 -0700

    Updates

--- path----41ea053bcfbc33f35827.js
+++ path----41ea053bcfbc33f35827.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{103:function(t,e){t.exports={data:{site:{siteMetadata:{title:"vine & fork"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{101:function(t,e){t.exports={data:{site:{siteMetadata:{title:"vine & fork"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----41ea053bcfbc33f35827.js.map
